29c3793ff84fc0f33b34187e2cc1e4420e2f3e84
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2024-09-17 00:54:55 -0300

    first

---
+++ Marcação de Atendimentos/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marcação_de_Atendimentos.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Marcação de Atendimentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marcação_de_Atendimentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
