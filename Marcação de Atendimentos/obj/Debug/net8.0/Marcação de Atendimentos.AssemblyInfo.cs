5db9c8daf900406a4dd085f07d508ce63856aab2
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2024-09-17 01:04:08 -0300

    first

--- Marcação de Atendimentos/obj/Debug/net8.0/Marcação de Atendimentos.AssemblyInfo.cs
+++ Marcação de Atendimentos/obj/Debug/net8.0/Marcação de Atendimentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marcação de Atendimentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c3793ff84fc0f33b34187e2cc1e4420e2f3e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marcação de Atendimentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marcação de Atendimentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
